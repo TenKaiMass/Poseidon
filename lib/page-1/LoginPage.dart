c5b3ca9fd8518b3c3bfb320b4e90f13786353722
TenKaiMass <mass.merouane@gamil.com>
2023-06-15 15:06:49 +0200

    auth end

--- lib/page-1/LoginPage.dart
+++ lib/page-1/LoginPage.dart
@@ -45,15 +45,24 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
-  Widget _Logo(BuildContext context){
-    double fem = MediaQuery.of(context).size.width / 393;
+  Widget _Logo() {
     return Container(
-      // cursiveitalicmultipurpsetypogr (29:203)
-      margin: EdgeInsets.fromLTRB(0 * fem, 120 * fem, 0 * fem, 117 * fem),
-      width: 180 * fem,
-      height: 180 * fem,
+      margin: const EdgeInsets.fromLTRB(40, 20, 40, 60),
+      width: 180,
+      height: 180,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.4),
+            spreadRadius: 0,
+            blurRadius: 30,
+            offset: const Offset(0, 20),
+          ),
+        ],
+      ),
       child: ClipRRect(
-        borderRadius: BorderRadius.circular(100 * fem),
+        borderRadius: BorderRadius.circular(100),
         child: Image.asset(
           'assets/page-1/images/cursive-italic-multipurpse-typography-logo-design-brand-identity-clean-comic-style-for-design-company-1.png',
           fit: BoxFit.cover,
@@ -69,51 +78,76 @@ class _LoginPageState extends State<LoginPage> {
     double baseWidth = 393;
     double fem = MediaQuery.of(context).size.width / baseWidth;
     double ffem = fem * 0.97;
+
     return TextField(
       controller: controller,
       decoration: InputDecoration(
         labelText: title,
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20 * fem),
-            ),
-            // hintText: 'email Id',
-            hintStyle: SafeGoogleFont(
-              'Poppins',
-              fontSize: 14 * ffem,
-              fontWeight: FontWeight.w500,
-              color: const Color(0x82ffffff),
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(25.0),
-              borderSide: const BorderSide(
-                color: Color(0x82ffffff),
-                width: 2.0,
-              ),
-            ),
+        labelStyle: const TextStyle(
+          color: const Color(0x82ffffff),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20 * fem),
+        ),
+        // hintText: 'email Id',
+        hintStyle: SafeGoogleFont(
+          'Poppins',
+          fontSize: 14 * ffem,
+          fontWeight: FontWeight.w500,
+          color: const Color(0x82ffffff),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(25.0),
+          borderSide: const BorderSide(
+            color: Color(0x82ffffff),
+            width: 2.0,
           ),
-          keyboardType: TextInputType.emailAddress,
-          style: SafeGoogleFont(
-            'Poppins',
-            fontSize: 13 * ffem,
-            fontWeight: FontWeight.w500,
-            height: 1.5 * ffem / fem,
-            color: const Color(0xffffffff),
-                    )
+        ),
+      ),
+      keyboardType: TextInputType.emailAddress,
+      style: SafeGoogleFont(
+        'Poppins',
+        fontSize: 13 * ffem,
+        fontWeight: FontWeight.w500,
+        height: 1 * ffem / fem,
+        color: const Color(0xffffffff),
+      ),
+      obscuringCharacter: '*',
+      obscureText: title == 'Mot de passe'? true : false,
     );
   }
 
   Widget _errorMessage() {
     return Text(errorMessage == '' ? '' : '$errorMessage',
         style: GoogleFonts.lato(
-          color: Colors.green,
+          color: Colors.red,
         ));
   }
 
   Widget _submitButton() {
+    double baseWidth = 393;
+    double fem = MediaQuery.of(context).size.width / baseWidth;
+    double ffem = fem * 0.97;
     return ElevatedButton(
       onPressed:
           isLogin ? signInWithEmailAndPassword : createUserWithEmailAndPassword,
-      child: Text(isLogin ? 'Connexion' : "s'enregistrer"),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color(0xfff88c70),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20 * fem),
+        ),
+        minimumSize: Size(300 * fem, 50 * fem),
+      ),
+      child: Text(
+        isLogin ? 'Connexion' : "s'enregistrer",
+        style: SafeGoogleFont(
+          'Poppins',
+          fontSize: 17 * ffem,
+          fontWeight: FontWeight.w500,
+          height: 1.5,
+          color: const Color(0xffffffff),
+        ),
+      ),
     );
   }
 
@@ -124,13 +158,18 @@ class _LoginPageState extends State<LoginPage> {
           isLogin = !isLogin;
         });
       },
-      child: Text(isLogin ? 'Crée un compte' : 'Déjà un compte'),
+      child: Text(
+        isLogin ? 'Crée un compte' : 'Déjà un compte',
+        style: SafeGoogleFont(
+          'Poppins',
+          color: const Color(0xffffffff),
+        ),
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: Container(
         padding: const EdgeInsets.fromLTRB(40, 8, 40, 20),
@@ -152,8 +191,9 @@ class _LoginPageState extends State<LoginPage> {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            _Logo(context),
-            _entryField('email', _controllerEmail),
+            _Logo(),
+            _entryField('email Id', _controllerEmail),
+            Container(padding: const EdgeInsets.fromLTRB(40, 8, 40, 20)),
             _entryField('Mot de passe', _controllerPassword),
             _errorMessage(),
             _submitButton(),
